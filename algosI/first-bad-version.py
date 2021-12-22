001d912174e612a15045d1047c6085efd683f066
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2021-12-22 06:58:02 -0800

    bad version done

--- algosI/first-bad-version.py
+++ algosI/first-bad-version.py
@@ -3,9 +3,20 @@
 # @return an integer
 # def isBadVersion(version):
 
+def isBadVersion(mid):
+    pass
+
 class Solution:
     def firstBadVersion(self, n: int):
         """
         :type n: int
         :rtype: int
         """
+        l, r = 1, n
+        while l < r:
+            mid = l + (r - l) // 2
+            if (isBadVersion(mid)):
+                r = mid
+            else:
+                l = mid + 1
+        return l
