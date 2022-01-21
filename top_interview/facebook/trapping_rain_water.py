ec6a626cf7731f87a091da2ef6ee73480ced32bd
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2022-01-21 08:05:13 -0700

    Merge k lists done

--- top_interview/facebook/trapping_rain_water.py
+++ top_interview/facebook/trapping_rain_water.py
@@ -1,15 +1,21 @@
-from typing import List, Optional
-
-def get_decreasing_sequence(height: List[int], start: int) -> Optional[int]:
-    j, last_index = start + 1, len(height) - 1
-    while j <= last_index and height[j] < height[start]:
-        j = j + 1
-
-
-
-def get_trough(height: List[int], start: int) -> Optional[(int, int)]:
-    pass
+from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        pass
+        left, right = 0, len(height) - 1
+        max_left, max_right = 0, 0
+        ans = 0
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= max_left:
+                    max_left = height[left]
+                else:
+                    ans = ans + (max_left - height[left])
+                left = left + 1
+            else:
+                if height[right] >= max_right:
+                    max_right = height[right]
+                else:
+                    ans = ans + (max_right - height[right])
+                right = right - 1
+        return ans
